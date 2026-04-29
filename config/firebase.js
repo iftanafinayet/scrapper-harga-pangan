38048d9e977b6d6bd98c6d66234d5b07ba85be56
Your Name <you@example.com>
2026-04-29 10:30:14 +0700

    feat: enable firebase service account configuration via environment variable

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,16 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-auth.json');
+
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    try {
+        serviceAccount = require('../firebase-auth.json');
+    } catch (e) {
+        console.error('Firebase service account not found. Please provide FIREBASE_SERVICE_ACCOUNT env var or firebase-auth.json file.');
+        throw e;
+    }
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
